871f2a96023a4f784e22589766689d169b3bf683
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-17 12:53:58 -0400

    while loops

---
+++ w1.js
@@ -0,0 +1,19 @@
+//While Loop reps
+
+//1. Write a while loop that will log in the console "ginger chocolate honey patties" 1000 times
+
+// var i = 0;
+
+// while (i < 100) {
+//     console.log("ginger chocolate honey paties")
+//     i += 1
+// }
+
+//2. Write another while loop that counts from 0 to 1000 and will log in the console the current loop number
+
+// let i = 0
+
+// while (i <= 1000) {
+//     console.log(i)
+//     i += 1
+// }
