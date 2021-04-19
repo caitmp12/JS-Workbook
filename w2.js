8bbe517dfe991a38b06b225cb4e27be1fdbcfab7
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-04-19 19:56:21 -0400

    w2 practice

---
+++ w2.js
@@ -0,0 +1,23 @@
+//For Loops
+
+//1. Write a for loop that counts from 0 to 100 and console.log each number
+
+for (i=0; i<=100; i++) {
+    console.log(i)
+}
+
+//2. Write antoehr for loop that counts from 7 to 635 
+
+for (i=7; i<= 635; i++) {
+    console.log(i)
+}
+
+//3. Declare a variable 
+let start = 0
+//Declare a variable
+const limit = 22
+//Write a loop that counts from the value of start to the value of limit, using those variables in the control panel of the loop
+
+for (start; start <= limit; start++) {
+    console.log(start)
+}
