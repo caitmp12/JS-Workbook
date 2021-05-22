e96fe4d38075338136358ded37432443ff4fdd40
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-05-21 21:01:12 -0400

    save

--- bk3.js
+++ bk3.js
@@ -1,8 +1,30 @@
-// 1. How do we assign a value to a variable?
+//Conceptual//
+// 1. How do we assign a value to a variable? - which line(s) is/are valid?
 
 let a = 0
 const b = 1
 // 9 = a
 // b = b + 5
 
-console.log(a)
+// console.log(a)
+
+//2. How do we change the value of a variable?
+// 3. How do we assign an existing variable to a new variable?
+
+a = b
+
+// console.log(a)
+
+// Strings //
+//1. Create a variable called firstVariable //2. assign it the value of a string //3. change the value of this variable to a number //4. store the value of firstVariable into a new variable called secondVariable //5. change the value of secondVariable to a string //6. What is the value of firstVariable?
+
+let firstVariable = "hello world"
+
+firstVariable = 2
+
+let secondVariable = firstVariable 
+
+secondVariable = "holaaaaa"
+
+// console.log(firstVariable)
+// console.log(secondVariable)
