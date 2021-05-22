a0d65b293c19b2c0f3876e0efb5e789bde7bd7b2
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2021-05-21 21:12:29 -0400

    save

--- bk3.js
+++ bk3.js
@@ -16,6 +16,7 @@ a = b
 // console.log(a)
 
 // Strings //
+//Switcharoo
 //1. Create a variable called firstVariable //2. assign it the value of a string //3. change the value of this variable to a number //4. store the value of firstVariable into a new variable called secondVariable //5. change the value of secondVariable to a string //6. What is the value of firstVariable?
 
 let firstVariable = "hello world"
@@ -28,3 +29,9 @@ secondVariable = "holaaaaa"
 
 // console.log(firstVariable)
 // console.log(secondVariable)
+
+// Strings - Combine It
+// 1. Create a variable called yourName and set it equal to your name as a string. // Write an expression that takes the string "Hello, my name is" and the variable yourName so that it returns a new string with them concatenated. 
+
+let yourName = "Caitlin"
+console.log("Hello, my name is " + yourName)
